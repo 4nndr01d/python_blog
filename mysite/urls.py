61b65fb3d3b8e517f03e52bcbd95bccba66d3623
4nndr01d <Super0rganism@yandex.ru>
2021-04-14 02:25:25 +0300

    Добавлена регистрация, начало добавления подтверждения пользователя по почту

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from . import views as core_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/signup/', core_views.signup, name='signup'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
